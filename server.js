b77f3c6b88576f047bce994db3277ea8e49fd576
Wenxue Zhao <ballista01@outlook.com>
2021-07-25 02:48:59 -0400

    database connected

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.use(cors())
-app.use(express.static('public'))
+const app = express();
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.once('open', () => console.log('database connected!'));
+
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
